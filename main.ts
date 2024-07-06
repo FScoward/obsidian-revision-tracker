2da6da00faa389af18d0ea0c42a8efd71e968510
FScoward <endlick@gmail.com>
2024-07-06 19:18:34 +0900

    init

--- main.ts
+++ main.ts
@@ -1,134 +1,92 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { DIFF_DELETE, DIFF_INSERT, diff_match_patch } from "diff-match-patch";
+import { Plugin, TFile } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class ObsidianRevisionTracker extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		console.log("Loading Obsidian Revision Tracker");
+		// テキストの差分を計算するコマンドを追加
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "calculate-diff",
+			name: "Calculate Text Diff",
+			callback: () => this.calculateDiff(),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		console.log("Unloading Obsidian Revision Tracker");
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	async calculateDiff() {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+			console.error("No active file found");
+			return;
+		}
+
+		const fileContent = await this.app.vault.read(activeFile);
+		const previousContent = await this.applyPatchToPreviousContent(
+			activeFile,
+			fileContent
+		);
+
+		const dmp = new diff_match_patch();
+		const diffs = dmp.diff_main(previousContent, fileContent);
+		dmp.diff_cleanupSemantic(diffs);
+
+		const patchList = dmp.patch_make(previousContent, fileContent);
+		const patchText = dmp.patch_toText(patchList);
+
+		await this.storePatch(activeFile, patchText);
+
+		diffs.forEach((part) => {
+			const color =
+				part[0] === DIFF_INSERT
+					? "green"
+					: part[0] === DIFF_DELETE
+					? "red"
+					: "grey";
+			console.log(`%c${part[1]}`, `color: ${color}`);
+		});
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	async getPatch(file: TFile): Promise<string> {
+		try {
+			const patchFile = this.app.vault.getAbstractFileByPath(
+				`${file.path}.patch.json`
+			);
+			if (patchFile instanceof TFile) {
+				return await this.app.vault.read(patchFile);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+		return "";
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	async storePatch(file: TFile, patch: string) {
+		const patchPath = `${file.path}.patch.json`;
+		try {
+			const patchFile = this.app.vault.getAbstractFileByPath(patchPath);
+			if (patchFile instanceof TFile) {
+				await this.app.vault.modify(patchFile, patch);
+			} else {
+				await this.app.vault.create(patchPath, patch);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-		containerEl.empty();
+	async applyPatchToPreviousContent(
+		file: TFile,
+		currentContent: string
+	): Promise<string> {
+		const patchText = await this.getPatch(file);
+		if (!patchText) return "";
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		const dmp = new diff_match_patch();
+		const patches = dmp.patch_fromText(patchText);
+		const [previousContent] = dmp.patch_apply(patches, currentContent);
+		return previousContent;
 	}
 }
